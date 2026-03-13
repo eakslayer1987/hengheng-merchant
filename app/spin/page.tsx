3cdb2dd2aa57ee1a834a11124ede9a9c700b57e8
eakslayer <dellth.charulak.sur@gmail.com>
2026-03-13 10:12:51 +0700

    fix: dynamic force-dynamic prerender

--- app/spin/page.tsx
+++ app/spin/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+export const dynamic = 'force-dynamic'
 import { useEffect, useRef, useState, useCallback } from 'react'
 import { useSearchParams, useRouter } from 'next/navigation'
 import { apiFetch, type SpinResult, type PrizeItem } from '@/lib/api'
